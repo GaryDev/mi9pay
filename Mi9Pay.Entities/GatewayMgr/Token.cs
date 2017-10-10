599d763bb657ede5f04720c703a7328655c83157
Gary Guo <shane8328@gmail.com>
2017-10-10 15:35:55 +0800

    Added entities

---
+++ Mi9Pay.Entities/GatewayMgr/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mi9Pay.Entities.GatewayMgr
+{
+    public class Token
+    {
+        public Guid UniqueId { get; set; }
+        public string AuthToken { get; set; }
+        public DateTime IssuedOn { get; set; }
+        public DateTime ExpiresOn { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Mi9Pay.Entities/GatewayMgr/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mi9Pay.Entities.GatewayMgr
+{
+    public class User
+    {
+        public User()
+        {
+            Tokens = new List<Token>();
+        }
+
+        public Guid UniqueId { get; set; }
+        public int UserId { get; set; }
+
+        public List<Token> Tokens { get; set; }
+    }
+}
